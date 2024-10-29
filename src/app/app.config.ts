edb1609e4bb5388289be156ef1f63821e198575b
Daniele Bordignon <daniele.bordignon@koerber.com>
2024-10-29 23:34:00 +0100

    pwa update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,14 @@ import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { routes } from './app.routes';
 import { AngularDelegate } from '@ionic/angular';
 import { provideIonicAngular, IonicRouteStrategy } from '@ionic/angular/standalone';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 // export function initializeFirebaseApp() {
 //   return initializeApp(environment.firebase);
@@ -29,7 +30,10 @@ export const appConfig: ApplicationConfig = {
 
     // provideFirestore(() => getFirestore()),
     // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideNoopAnimations(),
+    provideNoopAnimations(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
 
   ],
 };
