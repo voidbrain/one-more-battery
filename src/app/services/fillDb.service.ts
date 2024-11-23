b8e3fd84156d8625e40c88fa80281edc8c9c3746
voidbrain <daniele.bordignon@gmail.com>
2024-11-23 18:50:44 +0100

    FillDb update

--- src/app/services/fillDb.service.ts
+++ src/app/services/fillDb.service.ts
@@ -2634,7 +2634,61 @@ export class FillDbService {
     };
     this.db.putItem(dbTables['batteries-status'], itemStatus1);
 
-    // last update 2024-11-21
+    itemStatus1 = {
+      idBattery: 1,
+      date: new Date('2024-11-23'),
+      status: batteryStatusActionEnum.Charge,
+      enabled: +true,
+      deleted: +false,
+    };
+    this.db.putItem(dbTables['batteries-status'], itemStatus1);
+
+    itemStatus2 = {
+      idBattery: 2,
+      date: new Date('2024-11-23'),
+      status: batteryStatusActionEnum.Charge,
+      enabled: +true,
+      deleted: +false,
+    };
+    this.db.putItem(dbTables['batteries-status'], itemStatus2);
+
+    itemStatus3 = {
+      idBattery: 3,
+      date: new Date('2024-11-23'),
+      status: batteryStatusActionEnum.Charge,
+      enabled: +true,
+      deleted: +false,
+    };
+    this.db.putItem(dbTables['batteries-status'], itemStatus3);
+
+    itemStatus4 = {
+      idBattery: 4,
+      date: new Date('2024-11-23'),
+      status: batteryStatusActionEnum.Charge,
+      enabled: +true,
+      deleted: +false,
+    };
+    this.db.putItem(dbTables['batteries-status'], itemStatus4);
+
+    itemStatus1 = {
+      idBattery: 1,
+      date: new Date('2024-11-23'),
+      status: batteryStatusActionEnum.Discharge,
+      enabled: +true,
+      deleted: +false,
+    };
+    this.db.putItem(dbTables['batteries-status'], itemStatus1);
+
+    itemStatus1 = {
+      idBattery: 1,
+      date: new Date('2024-11-23'),
+      status: batteryStatusActionEnum.Charge,
+      enabled: +true,
+      deleted: +false,
+    };
+    this.db.putItem(dbTables['batteries-status'], itemStatus1);
+
+    // last update 2024-11-23
 
     console.info('[DB]: fill Db finish');
   }
