b5209ce9d9a99b45197b817c0cb7e53198f7f16b
Daniele Bordignon <daniele.bordignon@koerber.com>
2025-05-22 15:19:11 +0200

    app update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IonRouterOutlet, IonApp } from "@ionic/angular/standalone";
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [IonApp, IonRouterOutlet, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'evara-frontend';
+
+  constructor(private swUpdate: SwUpdate) {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe(() => {
+        // Prompt the user to reload the app
+        if (confirm('A new version of the app is available. Would you like to update?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
