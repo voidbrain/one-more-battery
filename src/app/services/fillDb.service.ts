63fa2d95d8efeb239ad705c580570bdc17a94460
Daniele Bordignon <daniele.bordignon@koerber.com>
2025-08-30 11:52:45 +0200

    Filldb Update

--- src/app/services/fillDb.service.ts
+++ src/app/services/fillDb.service.ts
@@ -9622,9 +9622,27 @@ export class FillDbService {
     };
     this.db.putItem(dbTables['batteries-status'], itemStatus8);
 
+    itemStatus7 = {
+      idBattery: 7,
+      date: new Date('2025-08-30'),
+      status: batteryStatusActionEnum.Charge,
+      enabled: +true,
+      deleted: +false,
+    };
+    this.db.putItem(dbTables['batteries-status'], itemStatus7);
+
+    itemStatus8 = {
+      idBattery: 8,
+      date: new Date('2025-08-30'),
+      status: batteryStatusActionEnum.Charge,
+      enabled: +true,
+      deleted: +false,
+    };
+    this.db.putItem(dbTables['batteries-status'], itemStatus8);
+
     console.info('[DB]: fill Db finish');
 
-    // last update 2025-08-26
+    // last update 2025-08-30
 
   }
 }
