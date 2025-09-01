6a46566a2df583b60882c7af61c1a453acf9f670
Daniele Bordignon <daniele.bordignon@koerber.com>
2025-09-01 15:04:05 +0200

    docker settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,37 +3,39 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { environment, Environment } from '../environments/environment';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { routes } from './app.routes';
 import { AngularDelegate } from '@ionic/angular';
 import { provideIonicAngular, IonicRouteStrategy } from '@ionic/angular/standalone';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideServiceWorker } from '@angular/service-worker';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
-// export function initializeFirebaseApp() {
-//   return initializeApp(environment.firebase);
-// }
+export function initializeFirebaseApp() {
+  const typedEnvironment: Environment = environment;
+  return initializeApp(typedEnvironment.firebase);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // AngularDelegate,
+    AngularDelegate,
+    { provide: FIREBASE_OPTIONS, useValue: (environment as Environment).firebase },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideIonicAngular({ mode: 'ios' }),
     provideRouter(routes),
     provideHttpClient(),
-
-    // provideAuth(() => getAuth()),
-
-    // provideFirestore(() => getFirestore()),
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideNoopAnimations(), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
-
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp((environment as Environment).firebase)),
+    provideMessaging(() => getMessaging()),
+    provideNoopAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
 };
